b28974c40333719a9e9f8f92165a320cacfbca7f
sunilyadavfabhotel <sunil.yadav@fabhotels.com>
2024-07-30 17:03:28 +0530

    submodule setup

--- App.tsx
+++ App.tsx
@@ -1,14 +1,71 @@
 // create a dummy app using nativewind
 
 import React from 'react';
-import {View, Text, Button, Alert} from 'react-native';
+import {Text, SafeAreaView, StatusBar, ScrollView} from 'react-native';
 import './global.css';
+
+import {
+  Container,
+  AlertModal,
+  ListComponent,
+  InputComponent,
+  DropdownComponent,
+  AutoSuggestComponent,
+  TableView,
+  ModalComponent,
+  Calendar,
+} from './rnwebcomponent/src';
 
 export default function App() {
   return (
-    <View className="bg-red-500 p-4">
-      <Text className="text-white text-lg">Hello World</Text>
-      <Button title="Click me" onPress={() => Alert.alert('Hello World')} />
-    </View>
+    <SafeAreaView>
+      <ScrollView className="z-1">
+        <StatusBar barStyle="dark-content" />
+
+        {/* Calender Components */}
+        <Container title="Calender">
+          <Calendar />
+        </Container>
+        {/* Text component */}
+        <Container title="Text">
+          <Text className="text-24 text-red-300">
+            React Native Web App Example
+          </Text>
+        </Container>
+
+        {/* List */}
+        <Container title="List">
+          <ListComponent />
+        </Container>
+
+        {/* Input field */}
+        <Container title="Enter some text">
+          <InputComponent />
+        </Container>
+
+        {/* Dropdown */}
+        <Container title="Dropdown">
+          <DropdownComponent />
+        </Container>
+
+        {/* Auto suggest */}
+        <Container title="Auto Suggest">
+          <AutoSuggestComponent />
+        </Container>
+
+        {/* Alert Modal */}
+        <Container title="Alert Modal">
+          <AlertModal />
+        </Container>
+
+        <Container title="Modal">
+          <ModalComponent />
+        </Container>
+
+        <Container title="Table View">
+          <TableView />
+        </Container>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
